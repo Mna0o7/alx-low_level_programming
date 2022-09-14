4bd01c1ab23ef69f87b1e6bf46a5e5adf8b6423c
Mna0o7 <immanuelkmwiti@gmail.com>
2022-09-14 02:37:26 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,22 +8,34 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int x = 0;
+	int a = 0, b = 0, c = 0, d = 0;
 
-	for (a = 48; a <= 50; a++)
+	while (x < 1440)
 	{
-		for (c = 48; c <= 53; c++)
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+		if (d > 9)
 		{
-			for (d = 48; d <= 57; d++)
-			{
-				if (a >= 50 ; b >= 52)
-					break;
-				_putchar(a);
-				_putchar(b);
-				_putchar(c);
-				_putchar(d);
-				_putchar('\n');
-			}
+			d = 0;
+			c++;
 		}
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		x++;
 	}
 }
