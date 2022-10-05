02a28f6e7ce3fea59921c018549e847a9ccd6deb
Mna0o7 <immanuelkmwiti@gmail.com>
2022-10-05 00:46:45 -0700

    What does argc and argv mean?

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - prints name of the program
+ * main - prints the number of arguments passed into it
  *
  * @argc: argument count
  * @argv: argument vector
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 /**
- * main - prints name of the program
+ * main - prints all arguments it receives
  *
  * @argc: argument count
  * @argv: argument vector
@@ -9,7 +8,9 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
